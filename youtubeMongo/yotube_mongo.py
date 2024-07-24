c90e8a3d0df4a04fb4ceb9f84a858d6d4e0e4e23
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-24 09:26:42 +0530

    update

--- youtubeMongo/yotube_mongo.py
+++ youtubeMongo/yotube_mongo.py
@@ -7,56 +7,55 @@ client=MongoClient("mongodb+srv://youtubepy:<youtubepy>@atlascluster.dopnweg.mon
 
 #not good practice
 
-db=client["ytmanager"]
-video_collection=db["videos"]
+print(client)
+db = client["ytmanager"]
+video_collection = db["videos"]
 
-#<<print(video_collection)
+# print(video_collection)
+
+def add_video(name, time):
+    video_collection.insert_one({"name": name, "time": time})
+
 def list_videos():
     for video in video_collection.find():
-        print(f"ID:{video['_id']},Name:{video['name']} and Time:{video['time']}")
-def add_video(name,time):
-    video_collection.insert_one({"name": name,"time":time})
+        print(f"ID: {video['_id']}, Name: {video['name']} and Time: {video['time']}")
 
-def update_video(video_id,new_name ,new_time):
-    video_collection.update_one(
-        {'_id':video_id},
-        {"$Set":{"name":new_name,"time":new_time}}
-        )
+def update_video(video_id, new_name, new_time):
+    video_collection.update_one({'_id': ObjectId(video_id)}, {"$set": {"name": new_name, "time": new_time}})
+
 def delete_video(video_id):
-    video_collection.delete_one({"_id",video_id})
+    video_collection.delete_one({"_id": video_id})
+    # TODO: debug this video_id problem
+
 
 def main():
     while True:
-        print("\n Youtube Manager App")
-        print("1  List all videos: ")
-        print("2  Add a  videos: ")
-        print("3  Upadte a  videos: ")
-        print("4  delete a  videos: ")
-        print("5  Exit App: ")
-        choice=input("Enter your Choice")
-        if choice =="1":
+        print("\n Youtube manager App")
+        print("1. List all videos")
+        print("2. Add a new videos")
+        print("3. Update a videos")
+        print("4. Delete a videos")
+        print("5. Exit the app")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
             list_videos()
-        elif choice =="2":
-            name = input("Enter a Video name: ")
-            time= input("Enter the time: ")
-            add_video(name,time)
-        elif choice =="3":
-
-            video_id=int(input("Enter Video id"))
-            name = input("Enter a Update Video name: ")
-            time= input("Enter the  Updated video time: ")
-            update_video(video_id,name,time)
-        elif choice =="4":
-            
-            video_id=int(input("Enter Video id"))
-            delete_video(video_id,)
-        
-        elif choice =="5":
-            break 
+        elif choice == '2':
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            add_video(name, time)
+        elif choice == '3':
+            video_id = input("Enter the video id to update: ")
+            name = input("Enter the updated video name: ")
+            time = input("Enter the updated video time: ")
+            update_video(video_id, name, time)
+        elif choice == '4':
+            video_id = input("Enter the video id to update: ")
+            delete_video(video_id, name, time)
+        elif choice == '5':
+            break
         else:
-            print("Invalid Choice")   
-        
+            print("Invalid choice")
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
